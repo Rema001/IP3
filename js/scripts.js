fd03c7aad3e3a206001e1cae6050f7fe7a73eceb
Rema001 <indohmike@gmail.com>
2020-07-27 13:58:53 +0300

    attempted toggle effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-  
+
 $(".btn_1").click(function(){
-     $("p.par_1").show();
-     $(".btn_1").hide();
+     $("p.par_1").toggle();
+     $(".btn_1").toggle();
  });
  $("p.par_1").click(function(){
-     $("p.par_1").hide();
-     $(".btn_1").show();
+     $("p.par_1").toggle();
+     $(".btn_1").toggle();
  });
   $(".btn_2").click(function(){
-      $("p.par_2").show();
-      $(".btn_2").hide();
+      $("p.par_2").toggle();
+      $(".btn_2").toggle();
   });
   $("p.par_2").click(function(){
-      $("p.par_2").hide();
-      $(".btn_2").show();
+      $("p.par_2").toggle();
+      $(".btn_2").toggle();
   });
   $(".btn_3").click(function(){
-      $("p.par_3").show();
-      $(".btn_3").hide();
+      $("p.par_3").toggle();
+      $(".btn_3").toggle();
   });
   $("p.par_3").click(function(){
-      $(".btn_3").show();
-      $("p.par_3").hide();
+      $(".btn_3").toggle();
+      $("p.par_3").toggle();
   });
 });
